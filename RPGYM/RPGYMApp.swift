22d784594c693536c7619ab61b4618145b1cbff5
xXinous <marcelop.smile@gmail.com>
2026-03-02 11:34:56 -0300

    Initial Commit

---
+++ RPGYM/RPGYMApp.swift
@@ -0,0 +1,17 @@
+//
+//  RPGYMApp.swift
+//  RPGYM
+//
+//  Created by Marcelo Pessoa on 02/03/26.
+//
+
+import SwiftUI
+
+@main
+struct RPGYMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
